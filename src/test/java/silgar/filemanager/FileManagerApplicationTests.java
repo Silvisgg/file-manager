b1d5b6fbbc0b56c054ad79b6d8c07b720ec037a6
silvis_sil <silvis.garcia@gmail.com>
2023-02-26 20:06:00 +0100

    first commit

---
+++ src/test/java/silgar/filemanager/FileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package silgar.filemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
