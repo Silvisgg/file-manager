af96c89c45e153ff4816961f4219be22d44e7a54
silvis_sil <silvis.garcia@gmail.com>
2023-03-19 20:35:07 +0100

    [FMS-Upload-4] Comments and Spring Doc

--- src/main/java/silgar/fmsuploadfile/controller/UpdateFileController.java
+++ src/main/java/silgar/fmsuploadfile/controller/UpdateFileController.java
@@ -3,15 +3,13 @@ package silgar.fmsuploadfile.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import silgar.fmsuploadfile.service.IUpdateFileService;
 
-@Controller
-//@RequestMapping(produces = {"application/json", "application/xml", "application/hal+json"})
+@RestController
+@RequestMapping(produces = {"application/json", "application/xml"})
 public class UpdateFileController {
 
     private final IUpdateFileService iUpdateFileService;
@@ -21,8 +19,11 @@ public class UpdateFileController {
         this.iUpdateFileService = updateFileService;
     }
 
+    /*
+    Upload a file to the Storage
+     */
     @PostMapping(path = "/upload")
-    @ResponseStatus(HttpStatus.CREATED)//?
+    @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
 
@@ -34,13 +35,19 @@ public class UpdateFileController {
         return ResponseEntity.ok().headers(headers).body("You successfully uploaded " + file.getOriginalFilename() + "!");
     }
 
-    @GetMapping("/alive/{text}")
+    /*
+    Check if controller is well configured and able to send a status and a response
+     */
+    @GetMapping("/correct/{text}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ResponseEntity<String> checkAlive(@PathVariable String text) {
+    public ResponseEntity<String> checkWellBehaviour(@PathVariable String text) {
         return ResponseEntity.ok().body(text);
     }
 
+    /*
+    Check if the application is running
+     */
     @GetMapping(value = "/status")
     @ResponseBody
     public String status() {
